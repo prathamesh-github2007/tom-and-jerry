d6260be3eb3e39abc1330cddd2520c02f2544a48
prathamesh-github2007 <76560466+prathamesh-github2007@users.noreply.github.com>
2021-01-22 19:09:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var canvas,bg;
+var together;
+var tom, tomImg1, tomImg2;
+var jerry, jerryImg1,jerryImg2
+
+function preload() {
+bg=loadImage("images/garden.png")
+tomImg1=loadImage("images/tomone.png")
+tomImag2=loadImage("images/tomtwo.png")
+jerryImg1=loadImage("images/jerryone.png")
+jerryImg2=loadImage("images/jerrytwo.png")
+jerryImg3=loadImage("image/jerryfour")
+}
+
+function setup(){
+    createCanvas(1000,800);
+    
+tom=createSprite(870,600);
+tom.addAnimation("tomsleeping",tomImg1);
+tom.scale=0.2;
+
+jerry=createSprite(200,600);
+jerry.addAnimation("jerrystanding",jerryImg1);
+jerry.scale=0.15;
+}
+
+function draw() {
+
+    background(bg);
+    
+if(tom.x - jerry.x < (tom.width - jerry.width)/2)
+{
+  tom.velocityX=0;
+  tom.addAnimation("tomLastImage",tomImg3)
+  tom.x=300;
+  tom.scale=0.15;
+  tom.changeAnimation("tomLastImage");
+  jerry.addAnimation("jerryLastImage",jerryImg3)
+  jerry.scale=0.15;
+  jerry.changeAnimation("jerryLastImage");
+}
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+ if(keyCode === LEFT_ARROW){
+   tom.velocityX = -5;
+   tom.addAnimation("tomRunning",tomImag2)
+   tom.changeAnimation("tomRunning");
+
+   jerry.addAnimation("jerryTeasing",jerryImg2);
+   jerry.frameDelay = 25;
+   jerry.changeAnimation("jerryTeasing");
+ }
+
+}
